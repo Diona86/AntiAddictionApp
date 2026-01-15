50811bd153a5a9f8bf7a8bc14a6b5fb391a9ea8d
Diona86 <157512490+Diona86@users.noreply.github.com>
2026-01-15 15:54:51 +0800

    firebase测试匿名登录（修改依赖）

---
+++ app/src/main/java/com/exampl/antiaddiction/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.exampl.antiaddiction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    RadioButton radioChild;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        radioChild=findViewById(R.id.radioChild);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        // 在 LoginActivity 的 btnLogin.setOnClickListener 内部：
+        btnLogin.setOnClickListener(v -> {
+            // 1. 获取选中的角色
+            String role = radioChild.isChecked() ? "child" : "parent";
+            Log.d("ANTI_LOG", "点击了登录按钮，角色是: " + role); // 打印日志
+            // 2. 匿名登录
+            auth.signInAnonymously()
+                    .addOnSuccessListener(authResult -> {
+                        String uid = auth.getCurrentUser().getUid();
+                        Log.d("ANTI_LOG", "Firebase 登录成功，uid: " + uid);
+                        // 3. 将用户信息存入 Firestore
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("uid", uid);
+                        user.put("role", role);
+
+                        FirebaseFirestore.getInstance().collection("users")
+                                .document(uid)
+                                .set(user)
+                                .addOnSuccessListener(aVoid -> {
+                                    // 4. 跳转到主界面
+                                    Log.d("ANTI_LOG","Firestroe写入成功");
+                                    Intent intent = new Intent(this, MainActivity.class);
+                                    intent.putExtra("USER_ROLE", role); // 把角色传给主界面
+                                    startActivity(intent);
+                                    finish();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Log.e("ANTI_LOG","Firestroe写入失败:"+e.getMessage());
+                                });
+                    })
+                    .addOnFailureListener(e ->
+                            Toast.makeText(this, "初始化失败: " + e.getMessage(), Toast.LENGTH_SHORT).show()
+                    );
+        });
+    }
+}
